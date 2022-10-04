f81ad50df378a7d0299fbdfbe6b3d8f8de2b538e
Muhammad Arbab Hussain Siddiqui <muhammad.arbab.hussain.siddiqui@gmail.com>
2022-10-04 11:44:17 +0500

    App Layout in Next.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
+import Footer from "../components/footer";
+import Header from "../components/header";
+import "../styles/layout.css";
+import "../styles/globals.css";
 // Import all global CSS here for instance importing bootstrap CSS into the project
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  if (Component.getLayout) {
+    return Component.getLayout(<Component {...pageProps} />);
+  }
+
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,9 @@
 import React from "react";
+import Footer from "../components/footer";
 
 const About = () => {
   return (
-    <div>
+    <div className="layout-content">
       <h1>About</h1>
       <h2>This is an H2 Tag which takes global styling</h2>
     </div>
@@ -10,3 +11,12 @@ const About = () => {
 };
 
 export default About;
+
+About.getLayout = function PageLayout(page) {
+  return (
+    <>
+      {page}
+      <Footer />
+    </>
+  );
+};
